db5d410bccbc0e5a538a4d7e234d763ab80faf95
adziadkiewicz <ad@intellit.pl>
2023-06-05 10:12:54 +0200

    CommitAlpha
    Added Linux agents, vars moved  do var subfolder

---
+++ var/variables.bash
@@ -0,0 +1,9 @@
+stor_es="/var/lib/opensearch"
+stor_es_arch="/mnt/glog-arch"
+stor_gl="/var/lib/graylog-server"
+stor_gl_journal="/var/lib/graylog-server/journal"
+
+stor_es_mountpoint="/mnt/glog-data"
+stor_es_arch_mountpoint="/mnt/glog-arch"
+stor_gl_mountpoint="/mnt/glog-data"
+stor_gl_journal_mountpoint="/mnt/glog-data"
